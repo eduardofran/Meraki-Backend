48a3e6fae988a4b1ec20a65ef726659bba5e7763
Ollebacx <dav.ollebac@gmail.com>
2020-04-30 18:16:49 +0100

    filter by country & city working

--- api/controllers/events.controller.js
+++ api/controllers/events.controller.js
@@ -6,8 +6,15 @@ module.exports = {
 }
 
 function getAllEvents (req, res) {
+  const filters = {}
+  if (req.query.country) {
+    filters.country = { $regex: `${req.query.country}`, $options: 'i' }
+  }
+  if (req.query.city) {
+    filters.city = { $regex: `${req.query.city}`, $options: 'i' }
+  }
   EventsModel
-    .find()
+    .find(filters)
     .then(response => res.json(response))
     .catch((err) => handleError(err, res))
 }
